b166a1cc7f14002782afd06ad9260a7aca22e2af
Faith-tanui <tishtanui7980@gmail.com>
2025-09-08 00:14:05 +0200

    Adding Olist Dashboard

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+st.set_page_config(layout="wide")
 st.title("Power BI Report in Streamlit")
+left_column, right_column = st.columns(2)
 
 # Embed Power BI iframe
-components.html(
-    """
-    <iframe width="1000" height="600"
-    src="https://app.powerbi.com/view?r=eyJrIjoiYWE2NzkyNzMtMTdmOC00YjI2LWFjMGItYTNmOWE5YzdhZDJjIiwidCI6IjEyODJlZTRlLTE5YWItNDU0Mi1hN2IyLTU2MWIyNjIwNWRlMCJ9"
-    frameborder="0" allowFullScreen="true"></iframe>
-    """,
-    height=650,
-)
-
-components.html(
-    """
-    <iframe title="SUPERSTORE SALES" width="600" height="373.5" 
-    src="https://app.powerbi.com/view?r=eyJrIjoiYWE2NzkyNzMtMTdmOC00YjI2LWFjMGItYTNmOWE5YzdhZDJjIiwidCI6IjEyODJlZTRlLTE5YWItNDU0Mi1hN2IyLTU2MWIyNjIwNWRlMCJ9" 
-    frameborder="0" allowFullScreen="true"></iframe>
-    """
-)
+with right_column:
+    components.html(
+        """
+        <iframe width="1000" height="600"
+        src="https://app.powerbi.com/view?r=eyJrIjoiYWE2NzkyNzMtMTdmOC00YjI2LWFjMGItYTNmOWE5YzdhZDJjIiwidCI6IjEyODJlZTRlLTE5YWItNDU0Mi1hN2IyLTU2MWIyNjIwNWRlMCJ9"
+        frameborder="0" allowFullScreen="true"></iframe>
+        """,
+        height=650,
+    )
+with left_column:
+    components.html(
+        """
+        <iframe title="Olist Market Place" width="1000" height="600" 
+        src="https://app.powerbi.com/view?r=eyJrIjoiODI5ZTI3NTYtNTJmYS00MWE1LWJiYmYtY2E1NjYzYzUyMmI5IiwidCI6IjEyODJlZTRlLTE5YWItNDU0Mi1hN2IyLTU2MWIyNjIwNWRlMCJ9"
+        frameborder="0" allowFullScreen="true"></iframe>
+        """,
+        height=650, width=1000
+    )
